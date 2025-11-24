5c8a673a056f76f22be29840e35a51d699738ba9
2mal3 <56305732+2mal3@users.noreply.github.com>
2025-11-24 12:41:29 +0100

    chore: cleaner user shutdown

--- pytailwindcss_extra/__main__.py
+++ pytailwindcss_extra/__main__.py
@@ -8,6 +8,9 @@ if __name__ == "__main__":
     try:
         exit_code = main()
         sys_exit(exit_code)
+    except KeyboardInterrupt:
+        log.info("Exiting...")
+        sys_exit(0)
     except Exception as e:
         log.fatal(e)
         sys_exit(1)
